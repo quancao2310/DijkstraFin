fbbdd841f542b357fb75dcf10da1e72d18a5ec22
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-04-30 15:42:45 +0700

    add no records component

--- frontend/src/screens/Wallet/WalletScreen.tsx
+++ frontend/src/screens/Wallet/WalletScreen.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { SafeAreaView, Text, ScrollView, StyleSheet, View } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  ScrollView,
+  StyleSheet,
+  View,
+  Dimensions,
+} from "react-native";
 import DateFilterButton from "../../components/wallet/DateFilterButton";
 import MoneySource from "../../components/wallet/MoneySource";
 import ColorSystem from "../../color/ColorSystem";
 import AddNewSourceButton from "../../components/wallet/AddNewSourceButton";
+import NoRecord from "../../components/records/NoRecord";
 const tempData = [
   {
     name: "Visa",
@@ -45,13 +53,14 @@ export default function WalletScreen() {
           </ScrollView>
         </View>
         <AddNewSourceButton></AddNewSourceButton>
+        <NoRecord></NoRecord>
       </ScrollView>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   innerScroll: {
-    height: "65%",
+    height: 0.4 * Dimensions.get("window").height,
     borderBottomWidth: 1,
     borderColor: ColorSystem.primary[600],
   },
