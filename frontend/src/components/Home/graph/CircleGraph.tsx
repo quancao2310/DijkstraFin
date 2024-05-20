7b8e4a16e01807232fb3c21c77033707f1bef6b0
HOC HUYNH <hthaihoc2029@gmail.com>
2024-05-20 22:40:27 +0700

    create graph

---
+++ frontend/src/components/Home/graph/CircleGraph.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View } from "react-native";
+const CircleGraph = () => {
+  return (
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        width: "50%",
+        aspectRatio: 1,
+        borderColor: "#555",
+      }}
+    >
+      <Text>Biểu đồ HERE</Text>
+    </View>
+  );
+};
+
+export default CircleGraph;
--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,16 +1,121 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import CardBudget from "../components/Home/budget/CardBudget";
 import IconBudgetSystem from "../icon/IconBugetSystem";
 import IconGoalSystem from "../icon/IconGoalSystem";
+import ListCardBudget from "../components/Home/budget/ListCardBudget";
+import NoInfo from "../components/Home/budget/NoInfo";
+import { Feather, MaterialIcons } from "@expo/vector-icons";
+import ColorSystem from "../color/ColorSystem";
+import ModalAddBudget from "../components/Home/add/ModalAddBudget";
+import CircleGraph from "../components/Home/graph/CircleGraph";
+
+const budgets = [
+  { name: "restaurant", balance: 0 },
+  { name: "grocery", balance: 0 },
+  { name: "transport", balance: 0 },
+  { name: "cloth", balance: 0 },
+  { name: "beauty", balance: 0 },
+  { name: "bill", balance: 0 },
+  { name: "medication", balance: 0 },
+  { name: "entertainment", balance: 0 },
+];
+
 const HomeScreen = () => {
+  const [isAddBudgetModalVisible, setIsAddBudgetModalVisible] = useState(false);
+  const handleAddBudget = () => {
+    setIsAddBudgetModalVisible(true);
+  };
+  const handleAddTransaction = () => {};
   return (
     <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      style={[
+        styles.container,
+        isAddBudgetModalVisible
+          ? { backgroundColor: "rgba(0,0,0,0.3)" }
+          : { backgroundColor: "#fff" },
+      ]}
     >
-      <CardBudget icon={IconGoalSystem.investment} balance={0} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.containerview}>
+          <CircleGraph />
+          <View style={styles.addBudget}>
+            <Text style={{ fontSize: 20, fontWeight: "500" }}>Ngân sách</Text>
+            <TouchableOpacity
+              style={styles.buttonAddBudget}
+              onPress={handleAddBudget}
+            >
+              <MaterialIcons
+                name="add"
+                size={25}
+                color={ColorSystem.secondary[600]}
+              />
+              <Text style={{ fontSize: 18, color: ColorSystem.secondary[600] }}>
+                Thêm
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <ListCardBudget budgets={budgets} />
+          <NoInfo name="ngân sách" />
+
+          <View style={styles.addBudget}>
+            <Text style={{ fontSize: 20, fontWeight: "500" }}>Giao dịch</Text>
+            <TouchableOpacity
+              style={styles.buttonAddBudget}
+              onPress={handleAddTransaction}
+            >
+              <MaterialIcons
+                name="add"
+                size={25}
+                color={ColorSystem.secondary[600]}
+              />
+              <Text style={{ fontSize: 18, color: ColorSystem.secondary[600] }}>
+                Thêm
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <NoInfo name="giao dịch" />
+
+          <ModalAddBudget
+            isModalVisible={isAddBudgetModalVisible}
+            setIsModalVisible={setIsAddBudgetModalVisible}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  containerview: {
+    paddingHorizontal: "5%",
+  },
+  addBudget: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: "3%",
+  },
+  buttonAddBudget: {
+    flexDirection: "row",
+    alignItems: "center",
+    // backgroundColor: "",
+    padding: 10,
+  },
+});
 
 export default HomeScreen;
