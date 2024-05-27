c01ae0711a15826ea59756273abfe94e95fc4a87
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-05-27 22:55:30 +0700

    add moneySource api

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,8 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://dijkstrafin-backend.vercel.app/api/v1",
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState;
+      const token = state.LoginStatus.accessToken;
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
   }),
+  tagTypes: ["MoneySource"],
   endpoints: () => ({}),
 });
