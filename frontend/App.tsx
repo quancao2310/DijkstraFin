77b7a24db7e18a8e68112c624af25633dd6f33d7
Quan Cao <quancao2310@gmail.com>
2024-05-30 02:31:12 +0700

    Set up EAS Build

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,16 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
 import AppMenuProvider from "./src/navigation/AppMenuProvider";
 import Toast from "react-native-toast-message";
+import * as Sentry from '@sentry/react-native';
+
 const Stack = createStackNavigator();
+Sentry.init({
+  dsn: "https://221748e12638aaeb8cdb4625acef036c@o4507321111674880.ingest.us.sentry.io/4507321115082752",
+});
 
 const App = () => {
   return (
@@ -17,4 +21,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.wrap(App);
