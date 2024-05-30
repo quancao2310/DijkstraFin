1da346db8e03a782ebee20ffbcce48f62e08ae2e
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-05-30 12:36:16 +0700

    fix api call to user only

--- frontend/src/screens/Wallet/WalletScreen.tsx
+++ frontend/src/screens/Wallet/WalletScreen.tsx
@@ -13,19 +13,22 @@ import MoneySource from "../../components/wallet/MoneySource";
 import ColorSystem from "../../color/ColorSystem";
 import AddNewSourceButton from "../../components/wallet/AddNewSourceButton";
 import NoRecord from "../../components/records/NoRecord";
-import RecordCard from "../../components/records/RecordCard";
 import AddMoneySrcModal from "../../components/modals/AddMoneySourceModals";
-import { useGetAllSourceQuery } from "../../services/moneySources";
 import { mapNameToIcon } from "../../utils/mapIcon";
 import { useAppSelector } from "../../hooks/redux";
 import { RootState } from "../../store";
 import { WaitingIndicator } from "../../components/utils/WaitingIndicator";
-import { useGetRecordsQuery } from "../../services/records";
 import TransactionCard from "../../components/Home/add/transaction/TransactionCard";
+import {
+  useGetUserMoneySourcesQuery,
+  useGetUserRecordsQuery,
+} from "../../services/users";
 
 export default function WalletScreen() {
-  let { data: moneySources, isLoading } = useGetAllSourceQuery();
-  let { data: records, isLoading: isLoadingRecords } = useGetRecordsQuery();
+  const userId = useAppSelector((state: RootState) => state.LoginStatus.userId);
+  let { data: moneySources, isLoading } = useGetUserMoneySourcesQuery(userId);
+  let { data: records, isLoading: isLoadingRecords } =
+    useGetUserRecordsQuery(userId);
   let recordsData = [];
   if (!isLoadingRecords) {
     recordsData = records.map((item) => {
@@ -68,12 +71,22 @@ export default function WalletScreen() {
           </ScrollView>
         </View>
         <AddNewSourceButton />
-        {(isLoadingRecords || records.length == 0) && <NoRecord></NoRecord>}
-        {/* {!isLoadingRecords &&
-          records.length > 0 &&
-          recordsData.map((item, index) => {
-            return <TransactionCard record={item} key={index} />;
-          })} */}
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          {(isLoadingRecords || records.length == 0) && <NoRecord></NoRecord>}
+          <View style={{ width: "96%", justifyContent: "center" }}>
+            {records !== undefined &&
+              records.length > 0 &&
+              records
+                .filter(
+                  (item) => item.type === "income" || item.type === "expense"
+                )
+                .map((item, index) => {
+                  return <TransactionCard record={item} key={index} />;
+                })}
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
